46c6cf4bcc08b6a45c1bb9dc6744cd2ccbecb257
FS <florin.schwappach@ur.de>
2019-10-15 14:50:53 +0200

    Updated solutions for new GA API

--- src/BouncingBall/BouncingBall.java
+++ src/BouncingBall/BouncingBall.java
@@ -3,6 +3,7 @@ package BouncingBall;
 import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
+import de.ur.mi.oop.graphics.Circle;
 
 public class BouncingBall extends GraphicsApp {
 
@@ -11,6 +12,17 @@ public class BouncingBall extends GraphicsApp {
     private static final int CANVAS_WIDTH = 800;
     private static final int FRAME_RATE = 60;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
+    private static final int BALL_RADIUS = 15;
+    private static final int BALL_DIAMETER = 2 * BALL_RADIUS;
+    private static final float SPEED_REDUCTION_BOUNCE = 0.9f;
+    private static final float X_SPEED = 1;
+    private static final float INITIAL_YSPEED = 0;
+    private static final float GRAVITY = 0.15f;
+
+    /* Private instance variables */
+    private Circle ball; /* The ball object */
+    private float dx; /* Velocity delta in the x direction */
+    private float dy; /* Velocity delta in the y direction */
 
     /*
      * This method is called once when the program is started.
@@ -19,6 +31,19 @@ public class BouncingBall extends GraphicsApp {
     @Override
     public void initialize() {
         setupCanvas();
+        setupBall();
+    }
+
+    private void setupBall() {
+        ball = new Circle(0, 0, BALL_DIAMETER, Colors.RED);
+        // Set the initial speed for the ball
+        dx = X_SPEED;
+        dy = INITIAL_YSPEED;
+    }
+
+    private void setupCanvas() {
+        setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+        setFrameRate(FRAME_RATE);
     }
 
     /*
@@ -28,10 +53,35 @@ public class BouncingBall extends GraphicsApp {
     @Override
     public void draw() {
         drawBackground(BACKGROUND_COLOR);
+        if (ball.getXPos() + BALL_RADIUS < getWidth()) {
+            moveBall();
+            checkForCollision();
+        }
     }
 
-    private void setupCanvas() {
-        setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
-        setFrameRate(FRAME_RATE);
+    // Update, move and draw the ball
+    private void moveBall() {
+        dy += GRAVITY;
+        ball.move(dx, dy);
+        ball.draw();
+    }
+
+    /*
+     * Determine if collision with floor, update velocities and location as
+     * appropriate.
+     */
+    private void checkForCollision() {
+        // if ball hits the floor
+        if (ball.getYPos() + BALL_RADIUS > getHeight()) {
+            // reverse y-direction to bounce upwards and reduce speed
+            dy = -dy * SPEED_REDUCTION_BOUNCE;
+
+            // assume bounce will move ball an amount above the
+            // floor equal to the amount it would have dropped
+            // below the floor.
+            float diff = ball.getYPos() - (getHeight() - BALL_RADIUS);
+            ball.move(0, -2 * diff);
+            ball.draw();
+        }
     }
 }
